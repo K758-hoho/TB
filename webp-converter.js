e56e8c3a6a138aa9fa832ed979daf2aab33ddd45
K758-hoho <137480456+K758-hoho@users.noreply.github.com>
2024-12-17 15:36:54 +0700

    update webp-converter.js

--- webp-converter.js
+++ webp-converter.js
@@ -1,27 +1,33 @@
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script>
 $(document).ready(function() {
     const convertToWebP = () => {
-        const comicImages = $('.comicpage img, #cc-comic img');
+        // Target your specific comic structure
+        const comicImages = $('.comicimagewrap img');
         
         comicImages.each(function() {
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
-            
-            const tempImage = new Image();
-            tempImage.crossOrigin = "Anonymous";
-            tempImage.src = $(this).attr('src');
-            
-            tempImage.onload = () => {
-                canvas.width = tempImage.width;
-                canvas.height = tempImage.height;
-                ctx.drawImage(tempImage, 0, 0);
+            if(this.src.match(/\.(jpg|jpeg|png)$/i)) {
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
                 
-                canvas.toBlob(blob => {
-                    const webpUrl = URL.createObjectURL(blob);
-                    $(this).attr('src', webpUrl);
-                }, 'image/webp', 0.8);
-            };
+                const tempImage = new Image();
+                tempImage.crossOrigin = "Anonymous";
+                tempImage.src = this.src;
+                
+                tempImage.onload = () => {
+                    canvas.width = tempImage.width;
+                    canvas.height = tempImage.height;
+                    ctx.drawImage(tempImage, 0, 0);
+                    
+                    canvas.toBlob(blob => {
+                        const webpUrl = URL.createObjectURL(blob);
+                        $(this).attr('src', webpUrl);
+                    }, 'image/webp', 0.8);
+                };
+            }
         });
     };
 
-    setTimeout(convertToWebP, 100);
+    setTimeout(convertToWebP, 500);
 });
+</script>
