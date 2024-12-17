ba22b536c34ef2924c5ad20ec8d657ecf51b7216
K758-hoho <137480456+K758-hoho@users.noreply.github.com>
2024-12-17 07:38:34 +0700

    update all.js

--- all.js
+++ all.js
@@ -33,4 +33,23 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+/*Comic Reading Progress*/
+window.onload = function() {
+    let progressBar = document.querySelector('.progress-bar');
+    let comicTitleBox = document.querySelector('#comic-title-box');
 
+    if (progressBar && comicTitleBox) {
+        window.addEventListener('scroll', () => {
+            let titleBoxRect = comicTitleBox.getBoundingClientRect();
+            let windowHeight = window.innerHeight;
+            let progress = 0;
+
+            if (titleBoxRect.top <= 0) {
+                let visibleHeight = Math.min(titleBoxRect.bottom, windowHeight);
+            }
+
+            progress = Math.min(Math.max(progress, 0), 100);
+            progressBar.style.width = progress + '%';
+        });
+    }
+};
